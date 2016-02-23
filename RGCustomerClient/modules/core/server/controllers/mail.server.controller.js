7517a54aefd392b808efd879f795687dda27c3f5
keanuk <keanukerr@gmail.com>
2016-02-23 04:31:57 -0500

    Fixed broken files from previous commit, made overall design more unified

--- RGCustomerClient/modules/core/server/controllers/mail.server.controller.js
+++ RGCustomerClient/modules/core/server/controllers/mail.server.controller.js
@@ -1,69 +1,66 @@
-var nodemailer = require('nodemailer'); 
+var nodemailer = require('nodemailer');
 var http = require('http');
-var info = require('../../../../env/local.js'); // If this doesn't work it's Chris' fault. 
+var info = require('../../../../env/local.js'); // If this doesn't work it's Chris' fault.
 
-function send_email() {   
+function send_email() {
 
-var name = 'Name goes here'; 
-var invName = 'Name of inviter goes here'; 
-var project = 'Project string goes here';  
-var mailTo = 'A Client <client@website.com>' // Follow the syntax exactly. Note below. 
+var name = 'Name goes here';
+var invName = 'Name of inviter goes here';
+var project = 'Project string goes here';
+var mailTo = 'A Client <client@website.com>'; // Follow the syntax exactly. Note below.
 
-var url = 'http://www.Rapid-Genomics.com/somepath/' + securityString; 
-var securityString = 'AxFvsE42Fr6gfFG5herf34#$gergg34rFDSFFB'; 
-// Ideas: there are 26 capital letters, 26 lowercase letters, 10 digits. 
-// So its (62)^(number of characters in string) possibilites. 
+var securityString = 'AxFvsE42Fr6gfFG5herf34#$gergg34rFDSFFB';
+var url = 'http://www.Rapid-Genomics.com/somepath/' + securityString;
+// Ideas: there are 26 capital letters, 26 lowercase letters, 10 digits.
+// So its (62)^(number of characters in string) possibilites.
 
 var transporter = nodemailer.createTransport(info.mailer.url);
 // This is a "connection url" to gmail's smtp server (Send mail transfer protocol)
-// Syntax:   'smtps://<Username@gmail.com>:password@smtp.gmail.com' 
+// Syntax:   'smtps://<Username@gmail.com>:password@smtp.gmail.com'
 
-// The Transport object uses these credentials to connect to google's smtp server. 
+// The Transport object uses these credentials to connect to google's smtp server.
 // SMTP stands for "Simple Mail Transfer Protocol."
-// It's just a simple server google has that lets you push email through it. 
+// It's just a simple server google has that lets you push email through it.
 
 // var transporter = nodemailer.createTransport({ service: "gmail", auth: { user: "rapidgenommailer@gmail.com", pass: "" } });
 // alternate syntax to connect to transporter, more transparent
 
-// IMPORTANT: For both the to and from, you need a specific syntax. 
-//            This is required directly by the google smtp server. 
-// 			  'Name <someone@website.com>' name is any string, <> are required. 
+// IMPORTANT: For both the to and from, you need a specific syntax.
+//            This is required directly by the google smtp server.
+// 			  'Name <someone@website.com>' name is any string, <> are required.
 
-var body = 	name + ',\n\nYou have been invited to a RAPiD Genomics project by ' 
+var body = 	name + ',\n\nYou have been invited to a RAPiD Genomics project by ' + invName + '.\n Project details: ' + project + '.\n\nPlease click here ' + 'to accept the invitation and register your user profile.\n\n' + url + '\n\n- The RAPiD Genomics Team';
+// Body is the (long) string that actually goes in the email. It can
+// take in variables and concatenate them with the rest of the email.
+// Also, it's parsed as HTML so \n doesn't work but <tags> do.
+
+/* var body = '<h2>' + name + '<h2>,\n\nYou have been invited to a RAPiD Genomics project by '
 			+ invName + '.\n Project details: ' + project + '.\n\nPlease click here ' +
 			'to accept the invitation and register your user profile.\n\n' + url +
-			'\n\n- The RAPiD Genomics Team'; 
-// Body is the (long) string that actually goes in the email. It can 
-// take in variables and concatenate them with the rest of the email. 
-// Also, it's parsed as HTML so \n doesn't work but <tags> do. 
-
-/* var body = '<h2>' + name + '<h2>,\n\nYou have been invited to a RAPiD Genomics project by ' 
-			+ invName + '.\n Project details: ' + project + '.\n\nPlease click here ' +
-			'to accept the invitation and register your user profile.\n\n' + url +
-			'\n\n- The RAPiD Genomics Team'; 
+			'\n\n- The RAPiD Genomics Team';
 */ // WIP
-			
+
 var mailOptions = {
     from: 'RG Mailer <rapidgenommailer@gmail.com>', // sender address
     to: mailTo, // list of receivers
     subject: 'You have been invited to a RAPiD Genomics Project', // Subject line
     html: body // html body
-	// This is just a big string that is parsed as an html. 
-	// We can use variables from above, which can be set from a webpage and such. 
-};  
+	// This is just a big string that is parsed as an html.
+	// We can use variables from above, which can be set from a webpage and such.
+};
 
 transporter.sendMail(mailOptions, function(error, info){
 	if(error) {
 		return console.log(error); }
-		}); 
+		});
 
-}; // End of send_email() function. 
+} // End of send_email() function.
 
-// DEBUG info 
-// Self signed certificate error: allow unauthorized access 
-// 			in windows node js prompt: SET NODE_TLS_REJECT_UNAUTHORIZED=0 
+// DEBUG info
+// Self signed certificate error: allow unauthorized access
+// 			in windows node js prompt: SET NODE_TLS_REJECT_UNAUTHORIZED=0
 // Not signed in error. It will give you a wall of text and URLs. Try signing into RGmailer
-// 			on your (chrome?) browser and try again. 
-// Response 555 error: This means there is a syntax error with the receive email or sender email. 
+// 			on your (chrome?) browser and try again.
+// Response 555 error: This means there is a syntax error with the receive email or sender email.
 
-// send_email(); // REMOVE THIS when we get it working with html requests. 
+// send_email(); // REMOVE THIS when we get it working with html requests.
