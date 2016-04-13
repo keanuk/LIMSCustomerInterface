b50ec52bbe61f3ee752bb4ef7f4007e62bf75b2c
keanuk <keanu@akua.com>
2016-04-13 01:16:04 -0400

    Minor stuff

--- RGCustomerClient/modules/core/client/controllers/home.client.controller.js
+++ RGCustomerClient/modules/core/client/controllers/home.client.controller.js
@@ -72,27 +72,27 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
 document.addEventListener('click', addRippleEffect, false);
 
 
-	var platedata = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]; 
-	
-	$scope.labels = ['Pending Arrival', 'Sample Arrived', 'Quality Control 1', 'Shearing', 
-			'Library Preparation', 'Quality Control 2', 'Hybridization', 'Quality Control 3', 
+	var platedata = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]];
+
+	$scope.labels = ['Pending Arrival', 'Sample Arrived', 'Quality Control 1', 'Shearing',
+			'Library Preparation', 'Quality Control 2', 'Hybridization', 'Quality Control 3',
 			'Sequencing', 'Data Analysis', 'Completed'];
     $scope.series = ['Series A'];
-	$scope.colors = 
+	$scope.colors =
 	[{
-			fillColor: "#b2b3b4", 
-            strokeColor: "#000000", 
-            highlightFill: "#ff9966", 
-            highlightStroke: "#000000", 
-	}] ; 
-    $scope.data = platedata; // Need to update this every time we get a new project. 
-	
+			fillColor: "#b2b3b4",
+            strokeColor: "#000000",
+            highlightFill: "#ff9966",
+            highlightStroke: "#000000",
+	}] ;
+    $scope.data = platedata; // Need to update this every time we get a new project.
 
-//fillColor: ["#b2b3b4", "#b2b3b4", "#b2b3b4", "#b2b3b4", "#b2b3b4", "#b2b3b4", 
+
+//fillColor: ["#b2b3b4", "#b2b3b4", "#b2b3b4", "#b2b3b4", "#b2b3b4", "#b2b3b4",
 	//		"#b2b3b4", "#b2b3b4", "#b2b3b4", "#b2b3b4", "#ff2b00"]
-// highlightFill: ["#ff9966", "#ff9966", "#ff9966", "#ff9966", "#ff9966", "#ff9966", 
-// 			"#ff9966", "#ff9966", "#ff9966", "#ff9966", "#ff8000"], 
-// The colors that they should be if they worked. 
+// highlightFill: ["#ff9966", "#ff9966", "#ff9966", "#ff9966", "#ff9966", "#ff9966",
+// 			"#ff9966", "#ff9966", "#ff9966", "#ff9966", "#ff8000"],
+// The colors that they should be if they worked.
 // DO NOT DELETE ANY COMMENTS!!!!!!!
 
     $scope.switchProject = function(x) {
@@ -102,24 +102,24 @@ document.addEventListener('click', addRippleEffect, false);
       $scope.platesAccess = false;
       $scope.projectAccess = false;
       $scope.projectFinancesAccess = false;
-
+// 
       $scope.currentProject = x;
-	  
+
 	  platedata[0] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-	  
+
 	  for (var q in $scope.currentProject.plates) {
-    		if ($scope.currentProject.plates[q].stage >= 11) 
+    		if ($scope.currentProject.plates[q].stage >= 11)
 				platedata[0][10]++;
 			else
 				platedata[0][$scope.currentProject.plates[q].stage]++;
-	  }  //temporary... 
-		
-	// This needs to be a double array like this because of the way angular-chart.js
-	// is set up. It wants to do multiple series of bars and we only need one. Thus, 
-	// platedata[0] is the way to go. 
+	  }  //temporary...
 
-		$scope.data = platedata; // Need to update this every time we get a new project. 
-	  
+	// This needs to be a double array like this because of the way angular-chart.js
+	// is set up. It wants to do multiple series of bars and we only need one. Thus,
+	// platedata[0] is the way to go.
+
+		$scope.data = platedata; // Need to update this every time we get a new project.
+
       $scope.dname = x.displayName;
       $scope.uname = x.username;
       $scope.currProjectCode = x.projectCode;
@@ -142,15 +142,15 @@ document.addEventListener('click', addRippleEffect, false);
       $scope.dateLastEdit = x.lastEdited;
       $scope.dateDue = x.due;
       $scope.dateCreate = x.created;
-      $scope.plate0Stg = x.plates[0].stage;
-      $scope.plate1Stg = x.plates[1].stage;
-      $scope.plate2Stg = x.plates[2].stage;
-      $scope.plate3Stg = x.plates[3].stage;
-      $scope.plate4Stg = x.plates[4].stage;
-      $scope.plate5Stg = x.plates[5].stage;
-      $scope.plate6Stg = x.plates[6].stage;
-      $scope.plate7Stg = x.plates[7].stage;
-      $scope.plate8Stg = x.plates[8].stage;
+      $scope.plate0Stg = x.plates[0].stage + 1;
+      $scope.plate1Stg = x.plates[1].stage + 1;
+      $scope.plate2Stg = x.plates[2].stage + 1;
+      $scope.plate3Stg = x.plates[3].stage + 1;
+      $scope.plate4Stg = x.plates[4].stage + 1;
+      $scope.plate5Stg = x.plates[5].stage + 1;
+      $scope.plate6Stg = x.plates[6].stage + 1;
+      $scope.plate7Stg = x.plates[7].stage + 1;
+      $scope.plate8Stg = x.plates[8].stage + 1;
 
       if($scope.shouldDisplayUsers === true){
         $scope.filterUsersByProject(x.projectCode);  // change the displayed users
